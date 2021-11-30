1ce68a0e93e44f194e17053d44f7e7badfa396e1
黄小白 <813671607@qq.com>
2021-11-30 16:38:57 +0800

    模板

--- src/main.js
+++ src/main.js
@@ -2,11 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 公共样式
+import '@/styles/index.scss'
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
